630d702a8356c884ea4d7b4d1c4f4158645cf1d4
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-10-03 12:07:39 +0200

    server proxy

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const request = require('request')
 const cors = require('cors')
 const express = require('express')
-const PORT = process.env.PORT || 3000
+
+require('dotenv').load()
 
 const app = express()
 
-const URL_BASE = 'https://comicvine.gamespot.com/api'
+const { PORT, URL_BASE } = process.env
 
 const options = {
   headers: {
@@ -20,7 +21,9 @@ app.use(cors())
 
 app.get('*', function (req, res) {
   const urlRequested = req.originalUrl
-  console.log('requesting: ' + urlRequested)
+  console.log('-'.repeat(20))
+  console.log(`→ Attempt ${++counter}`)
+  console.log('path requested = ' + urlRequested)
   if (urlRequested.includes('favicon.ico')) {
     res.status(500).send("Couldn't get a JSON!")
     return
@@ -29,28 +32,18 @@ app.get('*', function (req, res) {
   options.url = URL_BASE + urlRequested
 
   console.log(options.url)
-  console.log('attempt...')
-  console.log(++counter)
 
   if (cache[options.url]) {
-    console.log('from cache...')
-    res.json(cache[options.url])
+    console.log('💾 from cache...')
+    res.send(cache[options.url])
   } else {
     console.log('🔥 requesting: ' + options.url)
     request(options, function (error, response, body) {
       if (error) res.status(500).send('Something went wrong!')
-      try {
-        console.log('trying to PARSE...')
-        const parsedResult = JSON.parse(body)
-        cache[options.url] = parsedResult
-        console.log('from fresh request...')
-        res.json(parsedResult)
-      } catch (e) {
-        res.status(500).send("Couldn't get a JSON!")
-      }
+      cache[options.url] = body
+      res.send(body)
     })
   }
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}...`))
-
